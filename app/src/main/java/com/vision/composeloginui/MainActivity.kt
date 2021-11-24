103fdacd999ff19c4b6846624af162e07fb82815
abdullah951 <khanabdullah4289951@gmail.com>
2021-11-24 16:10:20 +0500

    Initial Push

--- app/src/main/java/com/vision/composeloginui/MainActivity.kt
+++ app/src/main/java/com/vision/composeloginui/MainActivity.kt
@@ -3,17 +3,21 @@ package com.vision.composeloginui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.Button
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -75,6 +79,7 @@ fun LoginUi() {
                     Color.LightGray,
                     shape = RoundedCornerShape(8.dp)
                 ),
+
             label = {
                 Text("Email",fontSize = 12.sp)
             },
@@ -90,7 +95,113 @@ fun LoginUi() {
                 unfocusedIndicatorColor = Color.Transparent)
         )
         Spacer(modifier = Modifier.height(10.dp))
+        TextField(
+            modifier = Modifier
+                .background(color = Color.White)
+                .height(60.dp)
+                .border(
+                    width = 1.dp,
+                    Color.LightGray,
+                    shape = RoundedCornerShape(8.dp)
+                ),
+            value = password,
+            label = {
+                Text("Password",fontSize = 12.sp)
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password
+            ),
+            onValueChange = { it->
+                password = it
+            },
+            shape = RoundedCornerShape(8.dp),
+            leadingIcon = {
+                Icon(
+                    Icons.Filled.Lock,
+                    "LockIcon"
+                )
+            },
+            colors = androidx.compose.material.TextFieldDefaults.textFieldColors(
+                backgroundColor = Color.White,
+                focusedIndicatorColor =  Color.White, //hide the indicator
+                unfocusedIndicatorColor = Color.Transparent)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { /*TODO*/ },
+            content={
+                Text(
+                    text = "Login",color = Color.White)
+            }
+        )
+        Spacer(modifier = Modifier.height(6.dp))
+        Text(
+            text = "Or, login with"
+        )
+        Spacer(modifier = Modifier.height(6.dp))
+        Row() {
+            Surface(
+                border = BorderStroke(
+                    1.dp,
+                    Color.DarkGray
+                ),
+                shape = RoundedCornerShape(5.dp),
+            ) {
+                Image(
+                    painter = painterResource(
+                        id = R.drawable.facebook
+                    ),
+                    contentDescription = "FacebookIcon",
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .width(40.dp)
+                )
 
+            }
+            Spacer(modifier = Modifier.width(6.dp))
+            Surface(
+                border = BorderStroke(
+                    1.dp,
+                    Color.DarkGray
+                ),
+                shape = RoundedCornerShape(5.dp),
+            ) {
+                Image(
+                    painter = painterResource(
+                        id = R.drawable.google
+                    ),
+                    contentDescription = "GoogleIcon",
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .width(40.dp)
+                )
+
+            }
+            Spacer(modifier = Modifier.width(10.dp))
+            Surface(
+                border = BorderStroke(1.dp, Color.DarkGray),
+                shape = RoundedCornerShape(5.dp),
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.apple),
+                    contentDescription = "AppleIcon",
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .width(40.dp)
+                )
+
+            }
+        }
+        Spacer(modifier = Modifier.height(6.dp))
+        Row() {
+            Text(
+                text = "New to Login? ",
+                style = MaterialTheme.typography.body1
+            )
+            Text(
+                text = "Register",
+                color = MaterialTheme.colors.primaryVariant
+            )
+        }
     }
 }
 
